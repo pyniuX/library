77022e68acdf4a9d87879e2ce1106fbc75beb0c7
krzysztof <krzysiek01pk@gmail.com>
2024-03-06 12:48:04 +0100

    models finished

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Person
+from .models import Book, Person, Rent
 
 admin.site.register(Person)
+admin.site.register(Book)
+admin.site.register(Rent)
--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,12 +1,62 @@
-from datetime import date
-
 from django.db import models
 
 
-class Person(models.Model):
-    """Person class for library users and book authors."""
+class IsActive(models.Model):
+    """
+    Parent class containing is_active bracket
+    for books existing in library and library users.
+    """
+
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Person(IsActive):
+    """
+    Person class for future users and book authors.
+    """
 
     name = models.CharField(max_length=15)
+    second_name = models.CharField(max_length=15, blank=True, null=True)
     surname = models.CharField(max_length=15)
     birth_date = models.DateField()
-    death_date = models.DateField(default=None)
+    death_date = models.DateField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} {self.surname}"
+
+
+class Book(IsActive):
+    """
+    Book class for all books.
+    """
+
+    authors = models.ManyToManyField(Person)
+    title = models.CharField(max_length=512)
+
+    def __str__(self) -> str:
+        return f"id:{self.id}, {self.title}"
+
+
+class Rent(models.Model):
+    """
+    Rent class for borrowed books and things connected.
+    """
+
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(Person, on_delete=models.CASCADE)
+    borrowed_date = models.DateField(auto_now_add=True)
+    return_date = models.DateField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"Book:{self.book.id} borrowed by user:{self.user.id}"
+
+
+# TODO: django debug toolbar /while debug true
+# TODO: django extensions /while debug true
+# TODO: rental efficiency zapytac
+# TODO: no renting already rented books /uniq_together
+# TODO: uniq_together books and users adding
+# TODO: rental available only for active persons
