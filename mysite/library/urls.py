f7a845cb455177c125eb281f64732826e79fe14f
krzysztof <krzysiek01pk@gmail.com>
2024-03-18 19:27:21 +0100

    views and templates wip

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
-from library.views import index
+from library import views
 
 app_name = "library"
-urlpatterns = [path("", index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("people", views.people, name="people"),
+    path("people/authors", views.authors, name="authors"),
+    path("people/users", views.users, name="users"),
+    path("books", views.books, name="books"),
+    path("rents", views.rents, name="rents"),
+]
--- mysite/library/views.py
+++ mysite/library/views.py
@@ -6,3 +6,25 @@ from .models import Person
 
 def index(request):
     return render(request, "library/index.html")
+
+
+def people(request):
+    return render(request, "library/people.html")
+
+
+def authors(request):
+    return render(request, "library/index.html")
+
+
+def users(request):
+    users_list = Person.objects.active()
+    context = {"users_list": users_list}
+    return render(request, "library/users.html", context)
+
+
+def books(request):
+    return render(request, "library/index.html")
+
+
+def rents(request):
+    return render(request, "library/index.html")
