1c72ae39bf1468d40e2b5ff2d6c5e9fccd68bccb
krzysztof <krzysiek01pk@gmail.com>
2024-03-20 17:14:46 +0100

    add user works

--- mysite/library/urls.py
+++ mysite/library/urls.py
@@ -1,19 +1,21 @@
 from django.urls import path
 from library import views
 
+from .models import Person
+
 app_name = "library"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("people", views.people, name="people"),
-    path("people/authors", views.authors, name="authors"),
-    path("people/users", views.users, name="users"),
+    path("people/", views.people, name="people"),
+    path("people/authors/", views.authors, name="authors"),
+    path("people/users/", views.users, name="users"),
     path(
-        "people/users/add",
-        views.UserAddView.as_view(success_url="../../"),
+        "people/users/add/",
+        views.UserAddView.as_view(success_url=f"../{Person.objects.last().id+1}/"),
         name="user_add",
     ),
-    path("people/users/<int:pk>", views.user_status, name="user_status"),
-    path("people/users/delete", views.user_delete, name="user_delete"),
-    path("books", views.books, name="books"),
-    path("rents", views.rents, name="rents"),
+    path("people/users/<int:person_id>/", views.user_status, name="user_status"),
+    path("people/users/delete/", views.user_delete, name="user_delete"),
+    path("books/", views.books, name="books"),
+    path("rents/", views.rents, name="rents"),
 ]
