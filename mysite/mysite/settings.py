f52db6de7796e598b1da52dd348c6f4ac399bfe3
krzysztof <krzysiek01pk@gmail.com>
2024-03-28 15:36:13 +0100

    wip

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = "django-insecure-)j6hhkbqq2a%b*9@@($_!9e$4$$#12&!uate47^na%$$ruapyg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["ec2-35-173-69-207.compute-1.amazonaws.com"]
+ALLOWED_HOSTS = [
+    "ec2-35-173-69-207.compute-1.amazonaws.com",
+    "library2.pythonanywhere.com",
+]
 
 
 # Application definition
