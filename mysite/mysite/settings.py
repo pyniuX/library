91159e22ac33a5baa1e5e840f3691185e41c8dbc
krzysztof <krzysiek01pk@gmail.com>
2024-03-28 11:24:03 +0100

    requirements

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-)j6hhkbqq2a%b*9@@($_!9e$4$$#12&!uate47^na%$$ruapyg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
